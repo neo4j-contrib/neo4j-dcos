26f19f83eb4a65ac256c44e7ba64043c4c85a3e0
Johannes Unterstein <junterstein@mesosphere.io>
2016-11-28 20:53:05 +0100

    added dyncamical configuration - still crontab is not working

--- dcos-neo4j-proxy/init-dcos-neo4j-nginx.sh
+++ dcos-neo4j-proxy/init-dcos-neo4j-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eu
 
 path="/etc/nginx/conf.d/default.conf"
-backup="/etc/nginx/conf.d/default.conf_backup"
+backup="/etc/nginx/default.conf_backup"
 url="${DCOS_NEO4J_DNS_ENTRY:-core-neo4j.marathon.containerip.dcos.thisdcos.directory}"
 user="${DCOS_NEO4J_USER:-}"
 pass="${DCOS_NEO4J_PASS:-}"
@@ -12,22 +12,25 @@ else
 fi
 
 # make backup of configuration file if no one is present
-if [ ! -e "$backup" ]; then
-	cp $path $backup
-fi
+cp -n $path $backup
 
 # try
 {
 	# find master and update nginx
-	response=`curl -s -k $auth -XPOST -H content-type:application/json -H accept:application/json https://$url:7473/db/data/transaction/commit -d'{"statements":[{"statement":"CALL dbms.cluster.routing.getServers()"}]}' | jq '.results[].data[].row[]'`
-	leader=`echo $response | jq -r '(.server[] | select(.role | contains("WRITE"))) | .addresses[0]'`
-	if [ ! -z $leader ]; then
-		# adapt proxy configuration to point only to master
-		echo `cat $backup | sed 's/http:.*$/http:\/\/$leader:7474\//g'`
-		cat $backup | sed 's/http:.*$/http:\/\/$leader:7474\//g' > $path
-		cat $path | sed 's/https:.*$/https:\/\/$leader:7473\//g' >> $path
-		service nginx reload
-	fi
+        response=`curl -s -k $auth -XPOST -H content-type:application/json -H accept:application/json https://$url:7473/db/data/transaction/commit -d'{"statements":[{"statement":"CALL dbms.cluster.overview()"}]}'`
+        leader=`echo $response | jq '.results[].data[].row | select(.[2] | contains("LEADER")) | .[1][]'`
+		leader_clean=`echo $leader | sed 's/"//g'`
+        if [ -z "$leader_clean" ]; then
+                echo "no leader found for logging"
+        else
+                # adapt proxy configuration to point only to master
+                mv $backup $path
+                http=`echo $leader | jq -r 'select(. | contains("http:"))'`
+                https=`echo $leader | jq -r 'select(. | contains("https:"))'`
+                sed -i "s%http:.*$%$http/%g" $path
+                sed -i "s%https:.*$%$https/%g" $path
+                service nginx reload
+        fi
 } || {
 	# restore backup if update fails
 	mv $backup $path

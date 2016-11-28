f990719d9d664a5b0fe5f0bb0716f228539bdadb
Johannes Unterstein <junterstein@mesosphere.io>
2016-11-28 18:43:56 +0100

    https://github.com/neo4j/docker-neo4j/pull/68 quick fix

--- dcos-neo4j-server/init-dcos-neo4j.sh
+++ dcos-neo4j-server/init-dcos-neo4j.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -eu
 
+# this should be removed when https://github.com/neo4j/docker-neo4j/pull/68 is merged
+setting() {
+    setting="${1}"
+    value="${2}"
+    file="neo4j.conf"
+
+    if [ -n "${value}" ]; then
+        if grep -q -F "${setting}=" conf/"${file}"; then
+            sed --in-place "s|.*${setting}=.*|${setting}=${value}|" conf/"${file}"
+        else
+            echo "${setting}=${value}" >>conf/"${file}"
+        fi
+    fi
+}
+
 # set heap based on marathon configuration
 mem=`echo $MARATHON_APP_RESOURCE_MEM | sed -e 's/.0//g'`
 export NEO4J_dbms_memory_heap_maxSize=$mem
@@ -12,6 +27,9 @@ export NEO4J_causalClustering_discoveryAdvertisedAddress=$ip":5000"
 export NEO4J_causalClustering_transactionAdvertisedAddress=$ip":6000"
 export NEO4J_causalClustering_raftAdvertisedAddress=$ip":7000"
 export NEO4J_dbms_advertisedAddress=$ip
+
+# this should be removed when https://github.com/neo4j/docker-neo4j/pull/68 is merged
+setting "dbms.connectors.default_advertised_address" "$NEO4J_dbms_advertisedAddress"
 
 # try until DNS is ready
 url="${DCOS_NEO4J_DNS_ENTRY:-core-neo4j.marathon.containerip.dcos.thisdcos.directory}"
